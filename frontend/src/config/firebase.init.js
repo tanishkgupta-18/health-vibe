518986cd62c4ceb8ffa909e370ed41d90de781ff
tanishkgupta-18 <144482075+tanishkgupta-18@users.noreply.github.com>
2024-11-08 23:43:26 +0530

    Initial commit for frontend

---
+++ frontend/src/config/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZfbxqsit7um5co-FHuvWKX6aO5ARMb-I",
+  authDomain: "music-cons.firebaseapp.com",
+  projectId: "music-cons",
+  storageBucket: "music-cons.appspot.com",
+  messagingSenderId: "224145904923",
+  appId: "1:224145904923:web:af37627e05613beccf2fbe"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
